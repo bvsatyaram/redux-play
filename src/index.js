7d599c754135195291884371ce95fd8e90e81c4c
BV Satyaram <bvsatyaram@gmail.com>
2016-11-28 01:25:26 +0530

    Redux Uni-directional Flow

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import 'babel-polyfill';
 import React from 'react';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from "./routes";
@@ -8,7 +10,11 @@ import routes from "./routes";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.css';
 
+const store = configureStore();
+
 render (
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.getElementById('app')
 );
