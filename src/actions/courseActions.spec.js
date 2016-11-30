c59090beb32bc6180ba63d06f61d556b0a7aaa93
BV Satyaram <bvsatyaram@gmail.com>
2016-12-01 05:09:35 +0530

    Test Thunks

--- src/actions/courseActions.spec.js
+++ src/actions/courseActions.spec.js
@@ -2,6 +2,9 @@ import expect from 'expect';
 import * as courseActions from './courseActions';
 import * as types from './actionTypes';
 
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+
 describe('Course Actions', () => {
   describe('createCourseSuccess', () => {
     it('creates a CREATE_COURSE_SUCCESS action', () => {
@@ -10,6 +13,25 @@ describe('Course Actions', () => {
       const expectedAction = {type: types.CREATE_COURSE_SUCCESS, course: course};
 
       expect(action).toEqual(expectedAction);
+    });
+  });
+});
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
+describe('Async Actions', () => {
+  it('creates BEGIN_AJAX_CALL and LOAD_COURSES_SUCCESS when courses are loaded', () => {
+    const expectedActions = [
+      {type: types.BEGIN_AJAX_CALL},
+      {type: types.LOAD_COURSES_SUCCESS, body: {courses: [{id: 'clean-code', title: 'Clean Code'}]}}
+    ];
+    const store = mockStore({courses: []}, expectedActions);
+
+    store.dispatch(courseActions.loadCourses()).then(() => {
+      const actions = store.getActions();
+      expect(actions[0].type).toEqual(types.BEGIN_AJAX_CALL);
+      expect(actions[1].type).toEqual(types.LOAD_COURSES_SUCCESS);
     });
   });
 });
